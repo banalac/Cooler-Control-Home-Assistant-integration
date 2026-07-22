4d8f9393a4f8629464e19ee9de3bf68345a3ebbd
banalac <banalac@yahoo.com>
2026-07-22 10:19:27 +0200

    Change default scan interval to 5 seconds
    Increased the default scan interval from 1 second to 5 seconds.

--- custom_components/coolercontrol/const.py
+++ custom_components/coolercontrol/const.py
@@ -7,7 +7,7 @@ CONF_VERIFY_SSL = "verify_ssl"
 
 DEFAULT_PORT = 11987
 DEFAULT_VERIFY_SSL = False
-DEFAULT_SCAN_INTERVAL = 1  # seconds
+DEFAULT_SCAN_INTERVAL = 5  # seconds
 
 # CoolerControl REST endpoints (daemon default port 11987, HTTPS w/ self-signed cert)
 ENDPOINT_DEVICES = "/devices"
